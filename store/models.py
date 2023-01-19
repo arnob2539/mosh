b2171f54efdae4a4f893d7b254d6301f3ba0010b
Md. Monjor Morshed <2020-1-60-227@std.ewubd.edu>
2023-01-19 21:18:19 +0600

    git commit

--- store/models.py
+++ store/models.py
@@ -58,6 +58,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    zip = models.IntegerField()
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
