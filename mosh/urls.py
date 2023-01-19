c951c07d519757c1439542b207f6aa9815c18173
Md. Monjor Morshed <2020-1-60-227@std.ewubd.edu>
2023-01-19 11:54:16 +0600

    git commit

--- mosh/urls.py
+++ mosh/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include('debug_toolbar.urls')),
-    path('playground/', include('playground.urls')),
+   
+    path('/playground/', include('playground.urls')),
+     path('__debug__/', include(debug_toolbar.urls)),
     
 ]
