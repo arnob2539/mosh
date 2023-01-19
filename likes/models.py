1c3407b3dfe2fc128782c04c019a6a0b64478f76
Md. Monjor Morshed <2020-1-60-227@std.ewubd.edu>
2023-01-19 20:47:10 +0600

    git commit

--- likes/models.py
+++ likes/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-#from store.models import Product
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
