cef4140a3ce4fd7b8d61bcbdb08033c24f2bc727
Md. Monjor Morshed <2020-1-60-227@std.ewubd.edu>
2023-01-20 09:14:11 +0600

    git commit

--- mosh/settings.py
+++ mosh/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = 'mosh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mosh',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'monjur.morshed@2789'
     }
 }
 
